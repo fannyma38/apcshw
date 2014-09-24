e591f70b9252f501d1584a28d93c4c09095db5c9
Fanny Ma <fannyma38@gmail.com>
2014-09-24 02:09:38 -0400

    Good Morning!

---
+++ 05-codingbat/Driver.java
@@ -0,0 +1,10 @@
+public class Driver {
+    public static void main(String[] args){
+	StringStuff s = new StringStuff();
+	System.out.println(s.nonStart("Coco", "Puffs"));
+	System.out.println(s.makeAbba("Fruit","Loops"));
+	System.out.println(s.diff21(2));
+    }
+   
+}
+
