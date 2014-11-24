1daac624cf8352fed071094d8a2fe36c3adc3296
fannyma38 <fannyma38@gmail.com>
2014-11-24 08:35:21 -0500

    Update Driver.java

--- 18-WordSearch/Driver.java
+++ 18-WordSearch/Driver.java
@@ -1,13 +1,13 @@
   public class Driver{
       public static void main(String[] args){
   	WordSearch ws = new WordSearch();
-  	ws.addWordH("candy","morning",4,3);
-  	ws.addWordV("crush", "please",12,2);
-  	ws.addWordD("yomama","yodada",1,2);
-  	ws.addWordD("peanut","sausage",17,14);
-  	ws.addWordH("gobble","ham",5,22);
-  	ws.addWordV("meepit","gurgle",8,21);
-  	ws.addWordD("commonapp","death",4,9);
+  	ws.addWordH("up","morning",4,3);
+  	ws.addWordV("southwest", "please",12,2);
+  	ws.addWordD("down","yodada",1,2);
+  	ws.addWordD("up","sausage",17,14);
+  	ws.addWordH("down","ham",5,22);
+  	ws.addWordV("down","gurgle",8,21);
+  	ws.addWordD("southwest","death",4,9);
   	System.out.println(ws);
       }
   }
